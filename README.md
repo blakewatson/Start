928a3f0049d60d854c829c1841d782fc5d152ee1
Blake Watson <blake@blakewatson.com>
2015-02-22 18:59:41 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Start
+
+Start is a replacement for the over-the-top, built-in “recent sites” pages that appear in web browsers when you open a new tab. I created Start because I just wanted a nice list of text links, organized into little groups.
+
+![Screenshot of Start](screenshot.png)
+
+## Getting Start
+
+The whole app is contained in the `start.html` file, so just grab that and save it somewhere. One handy trick is to put it in [Dropbox](http://dropbox.com/), that way you can get to it anywhere.
+
+If you want, you can use the [hosted version](http://start.blakewatson.com/start.html) at my website.
+
+The next step is to tell your web browser to use Start as your homepage and/or new tab page. The exact process varies by browser. Rather than use up a bunch of space here, I’ll simply point you to instructions on how to accomplish this step in various popular browsers:
+
+- [Safari](http://www.wikihow.com/Change-Your-Start-Page-on-Safari)
+- [Chrome (uses extension)](https://chrome.google.com/webstore/detail/replace-new-tab-page/cnkhddihkmmiiclaipbaaelfojkmlkja)
+- [Firefox](http://lifehacker.com/5916533/set-a-custom-new-tab-page-in-firefox)
+
+## Using Start
+
+The gist is that you add some bookmarks, which are saved in your web browser. Then those bookmarks are displayed to you in a nice click-friendly format every time you open a new browser tab.
+
+Start has these actions:
+
+### Add
+
+Click the add button to add a new bookmark. You can add it to an existing bundle or create a bundle. When creating a new bundle, you can also choose what column to add the bundle to.
+
+### Edit / Reorder
+
+When you click the “Edit / Reorder” button, you go into _reordering mode_. You can then drag and drop to reorder your bookmarks. Click and drag a bundle’s title to move the whole bundle.
+
+You can edit the name and URL of a bookmark by clicking it. You can also delete bookmarks by clicking the “delete key” icon that appears when hovering over a bookmark.
+
+When you are all finished, click “Done.”
+
+### Import / Export
+
+Because your bookmarks are saved directly in your browser, you will need to export them if you want to use them in another browser (or on another device). Click “Import / Export” to get a copy of your export code. Copy that code, then paste it into the import box of the other browser you want to use.
+
+## Developers
+
+I used the Mac app, CodeKit, to compile everything into a single HTML file. The `start.kit` file is where most of the magic happens. The Kit language allows for importing _any_ file, so `start.kit` grabs the CSS and JavaScript and compiles it all into `start.html`.
+
+### Backup data
+
+Whenever you import new bookmarks, the old ones get overwritten. While there’s no user-facing backup/restore functionality as of now, Start does make a backup of the current bookmarks before doing an import. Using your browser’s developer tools, you can manually retrieve the data from localStorage.
+
+The primary data key is “com.blakewatson.start”. The backup data key is “com.blakewatson.start.backup”. The values are JSON strings. They can be used as-is in an import. Just copy/paste.
---
+++ script.js
@@ -0,0 +1,326 @@
+// Bookmark class
+function Bookmark(args) {
+	var self = this;
+	self.name = ko.observable(args["name"]);
+	self.url = ko.observable(args["url"]);
+	//self.sub_name = ko.observable(args["sub_name"]);
+	//self.sub_url = ko.observable(args["sub_url"]);
+}
+
+// Column class
+function Column(args) {
+	var self = this;
+	self.name = args["name"];
+	self.bundles = ko.observableArray([]);
+}
+
+// Bundle class
+function Bundle(args) {
+	var self = this;
+	if(args["name"]=="") {
+		args["name"] = "Links";
+	}
+	self.name = args["name"];
+	self.bookmarks = ko.observableArray([]);
+}
+
+function StartViewModel() {
+	var self = this;
+	
+	// All the columns > bundles > bookmarks
+	self.columns = ko.observableArray([new Column({name: "Column 1"}), new Column({name: "Column 2"})]);
+	
+	// All the bundles > bookmarks
+	self.bundles = ko.computed(function() {
+		var bundles = [];
+		for(var i=0; i<self.columns().length; i++) {
+			var column = self.columns()[i];
+			for(var j=0; j<column.bundles().length; j++) {
+				bundles.push( column.bundles()[j] )
+			}
+		}
+		return bundles;
+	});
+	
+	// Get a specific bundle
+/*
+	self.getBundleByName = function(name) {
+		for(var i=0; i<self.bundles().length; i++) {
+			var bundle = self.bundles()[i];
+			if(bundle.name == name) {
+				return bundle;
+			}
+		}
+		return false
+	};
+*/
+	
+	/* -- New bookmark add-form --------------------------------- */
+	
+	// Input value for new bookmark name
+	self.newBookmarkName = ko.observable("");
+	
+	// Input value for new bookmark url
+	self.newBookmarkURL = ko.observable("");
+	
+	// Selected bundle in add form drop-down
+	self.selectedBundle = ko.observable("");
+	
+	// Checkbox for adding a new bundle
+	self.wantsNewBundle = ko.observable(false);
+	
+	// Input value for adding a new bundle
+	self.newBundleName = ko.observable("");
+	
+	// Selected column in add-form drop-down
+	self.selectedColumn = ko.observable("");
+	
+	// Flag for showing/hiding add form
+	self.showAddForm = ko.observable(false);
+	
+	// Function for showing/hiding add form
+	self.toggleAddForm = function() {
+		if(self.showAddForm()) {
+			self.showAddForm(false);
+		} else {
+			self.showAddForm(true);
+		}
+	};
+	
+	// Function determining whether form sufficiently completed
+	self.canAddBookmark = ko.computed(function() {
+		var verdict = false;
+		if(self.newBookmarkName() != "") {
+			if(self.newBookmarkURL() != "") {
+				if(self.selectedBundle() != "" && self.selectedBundle() != undefined) {
+					verdict = true;
+				} else if(self.newBundleName() != "") {
+					verdict = true;
+				}
+			}
+		}
+		return verdict;
+	});
+	
+	// Add a new bookmark
+	self.addBookmark = function() {
+		var bookmark = new Bookmark({
+			name: self.newBookmarkName(),
+			url: self.newBookmarkURL()
+		});
+		var bundle;
+		if(self.wantsNewBundle()) {
+			// add a new bundle
+			bundle = new Bundle({
+				name: self.newBundleName()
+			});
+			bundle.bookmarks.push(bookmark);
+			var column = self.selectedColumn();
+			column.bundles.push(bundle);
+		} else {
+			bundle = self.selectedBundle();
+			bundle.bookmarks.push(bookmark);
+		}
+		self.dumpStorage(); // Save changes!
+		self.clearAddForm();
+		self.toggleAddForm();
+	};
+	
+	// Reset the add form to blank values
+	self.clearAddForm = function() {
+		self.newBookmarkName("");
+		self.newBookmarkURL("");
+		self.selectedBundle("");
+		self.wantsNewBundle(false);
+		self.newBundleName("");
+		self.selectedColumn("");
+	};
+	
+	/* -- Editing and rearranging ------------------------------- */
+	
+	// Flag for reordering
+	self.isReordering = ko.observable(false)
+	
+	// Function for reordering edit mode
+	self.toggleEditMode = function() {
+		if(self.isReordering()) {
+			self.isReordering(false);
+		} else {
+			self.isReordering(true);
+		}
+	};
+	
+	// Just rearranged, check for empty bundles
+	self.afterMove = function() {
+		var columns = self.columns();
+		for(var i=0; i<columns.length; i++) {
+			var bundles = columns[i].bundles();
+			for(var k=0; k<bundles.length; k++) {
+				if(bundles[k].bookmarks().length < 1) {
+					self.deleteBundle(bundles[k], columns[i]);
+				}
+			}
+		}
+		self.dumpStorage();
+	};
+	
+	// Flag for editing a bookmark
+	self.isEditing = ko.observable(false);
+	
+	// Stop editing mode
+	self.stopEditing = function() {
+		// Hide the edit form
+		self.isEditing(false);
+		// Reset form values
+		self.updatedName("");
+		self.updatedURL("");
+		self.openedBookmark = {};
+	};
+	
+	// Show and prepopulate the edit form
+	self.startEditing = function(bookmark) {
+		// Set the currently opened bookmark
+		self.openedBookmark = bookmark;
+		// Populate text fields
+		self.updatedName(bookmark.name());
+		self.updatedURL(bookmark.url());
+		// Show edit form
+		self.isEditing(true);
+	}
+	
+	// Observables/properties for edit form values
+	self.updatedName = ko.observable("");
+	self.updatedURL = ko.observable("");
+	self.openedBookmark = {};
+	
+	// Update a bookmark
+	self.updateBookmark = function() {
+		var bookmark = self.openedBookmark;
+		// Make the changes
+		bookmark.name(self.updatedName());
+		bookmark.url(self.updatedURL());
+		// Hide edit form
+		self.stopEditing();
+		// Save to storage
+		self.dumpStorage();
+	};
+	
+	// Delete a bookmark
+	self.deleteBookmark = function(bookmark, parents) {
+		var bundle = parents[0];
+		bundle.bookmarks.remove(bookmark);
+		if(bundle.bookmarks().length < 1) {
+			self.deleteBundle(bundle, parents[1]);
+		}
+		self.dumpStorage();
+	};
+	
+	// Delete a bundle
+	self.deleteBundle = function(bundle, column) {
+		column.bundles.remove(bundle);
+	};
+	
+	/* -- Import / Export --------------------------------------- */
+	
+	// Flag
+	self.isImportExport = ko.observable(false);
+	
+	// Toggle for import / export view
+	self.toggleImportExport = function() {
+		if(self.isImportExport()) {
+			self.isImportExport(false);
+		} else {
+			self.isImportExport(true);
+		}
+	};
+	
+	// Observable that holds the value of the import textarea
+	self.importCode = ko.observable("");
+	
+	// Override local storage with new bookmarks
+	self.importBookmarks = function() {
+		if(self.importCode() !== "" && isJsonString(self.importCode())) {
+			// Make a backup
+			var current_storage = window.localStorage.getItem("com.blakewatson.start");
+			window.localStorage.setItem("com.blakewatson.start.backup", current_storage);
+			
+			// Overwrite main storage
+			window.localStorage.setItem("com.blakewatson.start", self.importCode());
+			
+			// Now load it
+			self.loadStorage();
+		}
+		self.importCode("");
+	};
+	
+	// Select export code to clipboard
+	self.selectExportCode = function(element) {
+		element.select()
+	};
+	
+	/* -- Interact with local storage --------------------------- */
+	
+	self.dumpStorage = function() {
+		var dump = ko.toJSON(self);
+		window.localStorage.setItem("com.blakewatson.start", dump);
+		return true;
+	};
+	
+	self.loadStorage = function() {
+		if( window.localStorage.getItem("com.blakewatson.start") ) {
+			var load = JSON.parse(window.localStorage.getItem("com.blakewatson.start"));
+			var columns = [];
+			// loop over columns
+			for(var i=0; i<load["columns"].length; i++) {
+				var this_column = load["columns"][i];
+				var new_column = new Column({name: this_column["name"]});
+				var bundles = [];
+				// loop over bundles
+				for(var k=0; k<this_column["bundles"].length; k++) {
+					var this_bundle = this_column["bundles"][k];
+					var bookmarks = [];
+					// loop over bookmarks
+					for(var z=0; z<this_bundle["bookmarks"].length; z++) {
+						var this_bookmark = this_bundle["bookmarks"][z];
+						var new_bookmark = new Bookmark({
+							name: this_bookmark["name"],
+							url: this_bookmark["url"]
+						});
+						bookmarks.push(new_bookmark);
+					}
+					var bundle = new Bundle({
+						name: this_bundle["name"]
+					});
+					bundle.bookmarks(bookmarks);
+					bundles.push(bundle);
+				}
+				new_column.bundles(bundles);
+				columns.push(new_column);
+			}
+			self.columns(columns);
+			return true;
+		} else {
+			return false;
+		}
+	};
+}
+
+var start = new StartViewModel();
+ko.applyBindings( start );
+start.loadStorage();
+
+$(document).ready(function() {
+	$(".loading").hide();
+	$(".appview").show();
+});
+
+/* -- Helpers ------------------------------------------------ */
+
+function isJsonString(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
